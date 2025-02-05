550c425925e149105f13d5e7ebba1ee4ee706f35
KotaTakeda <takeda11.kl@outlook.jp>
2025-02-05 15:47:24 +0900

    impl multiprocess

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from util import load_params, reduce_by_svd
 import pandas as pd
 from util import compute_edims
 import argparse
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 
 # ============================
@@ -23,7 +24,6 @@ def generate_trajectory(J, F, dt, N, data_dir=""):
     print(f"dt: {dt}")
 
     # N: number of time step, 1 year : 360*20
-    assert N > 360 * 20
     print(f"N: {N}")
 
     # Initial state near the stationary point
@@ -65,119 +65,293 @@ def generate_trajectory(J, F, dt, N, data_dir=""):
 # ============================
 
 
-def run_osse(
-    J, F, obs_per, dt, r, N_spinup, m_reduced_list, alpha_list, seeds, data_dir=""
-):
-    # Model parameter
-    p = (np.ones(J) * F,)
+# def run_osse(
+#     J, F, obs_per, dt, r, N_spinup, m_reduced_list, alpha_list, seeds, data_dir=""
+# ):
+#     # Model parameter
+#     p = (np.ones(J) * F,)
 
-    # Assimilation interval
-    Dt = obs_per * dt
+#     # Assimilation interval
+#     Dt = obs_per * dt
 
-    # Model function for EnKF
-    # def M(x, Dt):
-    #     for _ in range(int(Dt/dt)):
-    #         x = rk4(lorenz96, 0, x, p, dt)
-    #     return x
+#     # Model function for EnKF
+#     # def M(x, Dt):
+#     #     for _ in range(int(Dt/dt)):
+#     #         x = rk4(lorenz96, 0, x, p, dt)
+#     #     return x
 
-    def M_cython(x, Dt):
-        for _ in range(int(Dt / dt)):
-            x = rk4_cython(lorenz96_cython, 0, x, *p, dt)
+#     def M_cython(x, Dt):
+#         for _ in range(int(Dt / dt)):
+#             x = rk4_cython(lorenz96_cython, 0, x, *p, dt)
+#         return x
+
+#     # Observation
+#     H_diag = np.ones(J)
+#     H = np.diag(H_diag)
+#     print("diag of H:", H_diag)
+#     print("rank(H):", np.linalg.matrix_rank(H))
+
+#     # Model error covariance
+#     Q = np.zeros((J, J))  # not used
+
+#     # Observation error covariance
+#     print("r:", r)
+#     R = r**2 * np.eye(J)
+
+#     # Load true trajectory
+#     x_true = np.load(f"{data_dir}/x_true_l96.npy")
+
+#     # Initial ensemble size
+#     m0 = J + 1
+
+#     # ============================
+#     # Run OSSE
+#     # ============================
+#     Xa_dict = {}
+#     param_dict = {}
+#     filename = data_dir + "/{}-{}{}{}"
+
+#     # loop length
+#     print(f"{len(m_reduced_list)*len(alpha_list)*len(seeds)} loops")
+#     # param_list = []
+#     for i, m_reduced in enumerate(m_reduced_list):
+#         for j, alpha in enumerate(alpha_list):
+#             for k, seed in enumerate(seeds):
+#                 param_dict[(i, j, k)] = (m_reduced, alpha, seed)
+#                 # param_list.append(
+#                 #     {
+#                 #         "m_reduced": m_reduced,
+#                 #         "alpha": alpha,
+#                 #         "seed": seed,
+#                 #         "idx": (i, j, k),
+#                 #     }
+#                 # )
+#                 try:
+#                     Xa = np.load(filename.format("xa", i, j, k) + ".npy")
+#                     Xa_spinup = np.load(filename.format("xa_spinup", i, j, k) + ".npy")
+#                     Xa = [*Xa_spinup, *Xa]
+#                     print(filename.format("xa", i, j, k) + ".npy loaded")
+#                 except FileNotFoundError:
+#                     print(i, j, k)
+
+#                     np.random.seed(seed)
+#                     # generate obs
+#                     y = (H @ x_true.T).T
+#                     y += np.random.normal(loc=0, scale=r, size=y.shape)  # R = r^2*I
+
+#                     # generate initial ensemble
+#                     x_0 = x_true[np.random.randint(len(x_true) - 1)]
+#                     P_0 = 25 * np.eye(J)
+#                     X_0 = x_0 + np.random.multivariate_normal(
+#                         np.zeros_like(x_0), P_0, m0
+#                     )  # (m0, dim_x)
+
+#                     # run spin-up
+#                     print("spin-up")
+#                     etkf = ETKF(M_cython, H, R, alpha=alpha, store_ensemble=True)
+#                     etkf.initialize(X_0)
+#                     for y_obs in tqdm(y[:N_spinup]):
+#                         etkf.forecast(Dt)
+#                         etkf.update(y_obs)
+
+#                     # save spin-up data
+#                     np.save(filename.format("xf_spinup", i, j, k), etkf.Xf)
+#                     np.save(filename.format("xa_spinup", i, j, k), etkf.Xa)
+#                     Xa_spinup = etkf.Xa
+
+#                     # reduce ensemble
+#                     X_reduced = reduce_by_svd(etkf.X, m_reduced)  # by SVD
+#                     # X_reduced = reduce_by_sample(etkf.X, m_reduced) # by random sampling
+#                     etkf.initialize(X_reduced)
+#                     # etkf.alpha = 1.0
+#                     print("assimilation")
+#                     for y_obs in tqdm(y[N_spinup:]):
+#                         etkf.forecast(Dt)
+#                         etkf.update(y_obs)
+
+#                     # save data
+#                     np.save(filename.format("xf", i, j, k), etkf.Xf)
+#                     np.save(filename.format("xa", i, j, k), etkf.Xa)
+
+#                     Xa = [*Xa_spinup, *etkf.Xa]
+#                     del Xa_spinup, etkf
+#                 Xa_dict[(i, j, k)] = Xa
+#                 del Xa
+#     return Xa_dict, param_dict
+
+
+class OSSE:
+    def __init__(
+        self,
+        J,
+        F,
+        obs_per,
+        dt,
+        r,
+        N_spinup,
+        m_reduced_list,
+        alpha_list,
+        seeds,
+        data_dir="",
+    ):
+        # Model parameters
+        self.J = J
+        self.p = (np.ones(J) * F,)
+
+        # Assimilation interval
+        self.dt = dt
+        self.Dt = obs_per * dt
+
+        # Observation
+        H_diag = np.ones(J)
+        self.H = np.diag(H_diag)
+        print("diag of H:", H_diag)
+        print("rank(H):", np.linalg.matrix_rank(self.H))
+
+        # Model error covariance
+        self.Q = np.zeros((J, J))  # not used
+
+        # Observation error covariance
+        print("r:", r)
+        self.r = r
+        self.R = r**2 * np.eye(J)
+
+        # Load true trajectory
+        self.x_true = np.load(f"{data_dir}/x_true_l96.npy")
+
+        # Initial ensemble size
+        self.m0 = J + 1
+
+        # Number of spin-up steps
+        self.N_spinup = N_spinup
+
+        # m_reduced_list
+        self.m_reduced_list = m_reduced_list
+
+        # alpha_list
+        self.alpha_list = alpha_list
+
+        # seeds
+        self.seeds = seeds
+
+        # Filename format
+        self.filename = data_dir + "/{}-{}{}{}"
+
+    def M_cython(self, x, Dt):
+        N = int(Dt / self.dt)
+        for _ in range(N):
+            x = rk4_cython(lorenz96_cython, 0, x, *self.p, self.dt)
+        if Dt - N * self.dt > 0:
+            x = rk4_cython(lorenz96_cython, 0, x, *self.p, Dt - N * self.dt)
         return x
 
-    # Observation
-    H_diag = np.ones(J)
-    H = np.diag(H_diag)
-    print("diag of H:", H_diag)
-    print("rank(H):", np.linalg.matrix_rank(H))
+    def process(self, i, j, k, m_reduced, alpha, seed):
+        try:
+            Xa = np.load(self.filename.format("xa", i, j, k) + ".npy")
+            Xa_spinup = np.load(self.filename.format("xa_spinup", i, j, k) + ".npy")
+            Xa = [*Xa_spinup, *Xa]
+            print(self.filename.format("xa", i, j, k) + ".npy loaded")
+        except FileNotFoundError:
+            print(i, j, k)
 
-    # Model error covariance
-    Q = np.zeros((J, J))  # not used
+            np.random.seed(seed)
+            # generate obs
+            y = (self.H @ self.x_true.T).T
+            y += np.random.normal(loc=0, scale=self.r, size=y.shape)  # R = r^2*I
 
-    # Observation error covariance
-    print("r:", r)
-    R = r**2 * np.eye(J)
+            # generate initial ensemble
+            x_0 = self.x_true[np.random.randint(len(self.x_true) - 1)]
+            P_0 = 25 * np.eye(self.J)
+            X_0 = x_0 + np.random.multivariate_normal(
+                np.zeros_like(x_0), P_0, self.m0
+            )  # (m0, dim_x)
 
-    # Load true trajectory
-    x_true = np.load(f"{data_dir}/x_true_l96.npy")
+            # run spin-up
+            print("spin-up")
+            etkf = ETKF(self.M_cython, self.H, self.R, alpha=alpha, store_ensemble=True)
+            etkf.initialize(X_0)
+            for y_obs in tqdm(y[: self.N_spinup]):
+                etkf.forecast(self.Dt)
+                etkf.update(y_obs)
 
-    # Initial ensemble size
-    m0 = J + 1
+            # save spin-up data
+            np.save(self.filename.format("xf_spinup", i, j, k), etkf.Xf)
+            np.save(self.filename.format("xa_spinup", i, j, k), etkf.Xa)
+            Xa_spinup = etkf.Xa
+
+            # reduce ensemble
+            X_reduced = reduce_by_svd(etkf.X, m_reduced)  # by SVD
+            # X_reduced = reduce_by_sample(etkf.X, m_reduced) # by random sampling
+            etkf.initialize(X_reduced)
+            # etkf.alpha = 1.0
+            print("assimilation")
+            for y_obs in tqdm(y[self.N_spinup :]):
+                etkf.forecast(self.Dt)
+                etkf.update(y_obs)
+
+            # save data
+            np.save(self.filename.format("xf", i, j, k), etkf.Xf)
+            np.save(self.filename.format("xa", i, j, k), etkf.Xa)
+
+            Xa = [*Xa_spinup, *etkf.Xa]
+            del Xa_spinup, etkf
+        return Xa
 
     # ============================
     # Run OSSE
     # ============================
-    Xa_dict = {}
-    param_dict = {}
-    filename = data_dir + "/{}-{}{}{}"
+    def run(self, parallel="none"):
+        assert parallel in [
+            "none",
+            "mp",
+        ]  # none: no parallel, mp: multi process, mt is not supported
 
-    # loop length
-    print(f"{len(m_reduced_list)*len(alpha_list)*len(seeds)} loops")
-    param_list = []
-    for i, m_reduced in enumerate(m_reduced_list):
-        for j, alpha in enumerate(alpha_list):
-            for k, seed in enumerate(seeds):
-                param_dict[(i, j, k)] = (m_reduced, alpha, seed)
-                param_list.append(
-                    {
-                        "m_reduced": m_reduced,
-                        "alpha": alpha,
-                        "seed": seed,
-                        "idx": (i, j, k),
-                    }
-                )
-                try:
-                    Xa = np.load(filename.format("xa", i, j, k) + ".npy")
-                    Xa_spinup = np.load(filename.format("xa_spinup", i, j, k) + ".npy")
-                    Xa = [*Xa_spinup, *Xa]
-                    print(filename.format("xa", i, j, k) + ".npy loaded")
-                except FileNotFoundError:
-                    print(i, j, k)
+        Xa_dict = {}
+        param_dict = {}
+        # Loop
+        print(
+            f"{len(self.m_reduced_list)*len(self.alpha_list)*len(self.seeds)} loops"
+        )  # Loop size
+        if parallel == "none":
+            for i, m_reduced in enumerate(self.m_reduced_list):
+                for j, alpha in enumerate(self.alpha_list):
+                    for k, seed in enumerate(self.seeds):
+                        Xa_dict[(i, j, k)] = self.process(
+                            i, j, k, m_reduced, alpha, seed
+                        )
+                        param_dict[(i, j, k)] = (m_reduced, alpha, seed)
+        else:
+            executor = (
+                ProcessPoolExecutor() if parallel == "mp" else ThreadPoolExecutor()
+            )
+            with executor as exe:
+                features = []
+                param_list = []
+                for i, m_reduced in enumerate(self.m_reduced_list):
+                    for j, alpha in enumerate(self.alpha_list):
+                        for k, seed in enumerate(self.seeds):
+                            features.append(
+                                exe.submit(
+                                    self.process, i, j, k, m_reduced, alpha, seed
+                                )
+                            )
+                            param_list.append(
+                                {
+                                    "m_reduced": m_reduced,
+                                    "alpha": alpha,
+                                    "seed": seed,
+                                    "idx": (i, j, k),
+                                }
+                            )
+                for i, f in enumerate(features):
+                    Xa_dict[param_list[i]["idx"]] = f.result()
+                    param_dict[param_list[i]["idx"]] = (
+                        param_list[i]["m_reduced"],
+                        param_list[i]["alpha"],
+                        param_list[i]["seed"],
+                    )
 
-                    np.random.seed(seed)
-                    # generate obs
-                    y = (H @ x_true.T).T
-                    y += np.random.normal(loc=0, scale=r, size=y.shape)  # R = r^2*I
-
-                    # generate initial ensemble
-                    x_0 = x_true[np.random.randint(len(x_true) - 1)]
-                    P_0 = 25 * np.eye(J)
-                    X_0 = x_0 + np.random.multivariate_normal(
-                        np.zeros_like(x_0), P_0, m0
-                    )  # (m0, dim_x)
-
-                    # run spin-up
-                    print("spin-up")
-                    etkf = ETKF(M_cython, H, R, alpha=alpha, store_ensemble=True)
-                    etkf.initialize(X_0)
-                    for y_obs in tqdm(y[:N_spinup]):
-                        etkf.forecast(Dt)
-                        etkf.update(y_obs)
-
-                    # save spin-up data
-                    np.save(filename.format("xf_spinup", i, j, k), etkf.Xf)
-                    np.save(filename.format("xa_spinup", i, j, k), etkf.Xa)
-                    Xa_spinup = etkf.Xa
-
-                    # reduce ensemble
-                    X_reduced = reduce_by_svd(etkf.X, m_reduced)  # by SVD
-                    # X_reduced = reduce_by_sample(etkf.X, m_reduced) # by random sampling
-                    etkf.initialize(X_reduced)
-                    # etkf.alpha = 1.0
-                    print("assimilation")
-                    for y_obs in tqdm(y[N_spinup:]):
-                        etkf.forecast(Dt)
-                        etkf.update(y_obs)
-
-                    # save data
-                    np.save(filename.format("xf", i, j, k), etkf.Xf)
-                    np.save(filename.format("xa", i, j, k), etkf.Xa)
-
-                    Xa = [*Xa_spinup, *etkf.Xa]
-                    del Xa_spinup, etkf
-                Xa_dict[(i, j, k)] = Xa
-                del Xa
-    return Xa_dict, param_dict
+        return Xa_dict, param_dict
 
 
 # ============================
@@ -197,14 +371,14 @@ def summarize_results(m_reduced_list, alpha_list, seeds, N_spinup, T_inf, data_d
     rmse = np.zeros((len(m_reduced_list), len(alpha_list)))
     # traceP = np.zeros((len(m_reduced_list), len(alpha_list)))
     ensdim = np.zeros((len(m_reduced_list), len(alpha_list)))
-    for i, m_reduced in enumerate(m_reduced_list):
-        for j, alpha in enumerate(alpha_list):
+    for i, _ in enumerate(m_reduced_list):
+        for j, _ in enumerate(alpha_list):
             se_tmp = []
             rmse_tmp = []
             # traceP_tmp = []
             ensdim_tmp = []
             print(i, j)
-            for k, seed in enumerate(seeds):
+            for k, _ in enumerate(seeds):
                 Xa = np.load(filename.format("xa", i, j, k) + ".npy")
                 e = x_true[N_spinup:] - Xa.mean(axis=1)
 
@@ -238,8 +412,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--data_dir", type=str, default="data", help="Directory to save data"
     )
+    parser.add_argument(
+        "--parallel", type=str, default="none", help="Specify parallelization method"
+    )
     args = parser.parse_args()
     data_dir = args.data_dir
+    parallel = args.parallel
 
     # Use the function to load the parameters
     set_params = load_params(data_dir)
@@ -258,9 +436,21 @@ if __name__ == "__main__":
     seeds = set_params.seeds
 
     x_true = generate_trajectory(J, F, dt, N, data_dir)
-    Xa_dict, param_dict = run_osse(
-        J, F, obs_per, dt, r, N_spinup, m_reduced_list, alpha_list, seeds, data_dir
-    )
+    # Xa_dict, param_dict = run_osse(
+    #     J, F, obs_per, dt, r, N_spinup, m_reduced_list, alpha_list, seeds, data_dir
+    # )
+    Xa_dict, param_dict = OSSE(
+        J,
+        F,
+        obs_per,
+        dt,
+        r,
+        N_spinup,
+        m_reduced_list,
+        alpha_list,
+        seeds,
+        data_dir=data_dir,
+    ).run(parallel=parallel)
     df_sup_se, df_rmse, df_ensdim = summarize_results(
         m_reduced_list, alpha_list, seeds, N_spinup, T_inf, data_dir
     )
